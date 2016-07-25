39580717a879d376a2dc1bbc280075b2d7d847df
lainz <leandrodiazoe@gmail.com>
2016-07-25 16:23:27 -0300

    Fix compilation of LazPaint using Sytem.Round in FPC 3.1.1

--- lazpaint/uadjustcurves.pas
+++ lazpaint/uadjustcurves.pas
@@ -156,8 +156,8 @@ begin
   begin
       with CoordToBitmap(i/FNbGrid,i/FNbGrid) do
       begin
-        Bitmap.DrawLineAntialias(round(FPoint0.x),round(y),Bitmap.Width,round(y),FGridColor,BGRAPixelTransparent,FTickSize,false);
-        Bitmap.DrawLineAntialias(round(x),round(FPoint0.y),round(x),0,FGridColor,BGRAPixelTransparent,FTickSize,false);
+        Bitmap.DrawLineAntialias(System.round(FPoint0.x),System.round(y),Bitmap.Width,System.round(y),FGridColor,BGRAPixelTransparent,FTickSize,false);
+        Bitmap.DrawLineAntialias(System.round(x),System.round(FPoint0.y),System.round(x),0,FGridColor,BGRAPixelTransparent,FTickSize,false);
       end;
   end;
   curve := SelectedCurve;
@@ -519,14 +519,14 @@ begin
     with GetCoord(0,i/high(labels)) do
     begin
       Bitmap.TextOut(x-FTickSize,y-th div 2,labels[i],axesColor,taRightJustify);
-      Bitmap.DrawLine(round(x),round(y),round(x-FTickSize),round(y),axesColor,False);
+      Bitmap.DrawLine(System.round(x),System.round(y),System.round(x-FTickSize),System.round(y),axesColor,False);
     end;
     with GetCoord(i/high(labels),0) do
     begin
       if i = 0 then
         Bitmap.TextOut(x,y+FTickSize,labels[i],axesColor,taLeftJustify) else
         Bitmap.TextOut(x,y+FTickSize,labels[i],axesColor,taCenter);
-      Bitmap.DrawLine(round(x),round(y),round(x),round(y+FTickSize),axesColor,False);
+      Bitmap.DrawLine(System.round(x),System.round(y),System.round(x),System.round(y+FTickSize),axesColor,False);
     end;
   end;
   Bitmap.DrawLine(leftMargin, 0, leftMargin, Bitmap.Height - bottomMargin, axesColor, False);
