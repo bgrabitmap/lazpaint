c8d9e62fdbc0229a48f60c71a6b1d0bfa40513cf
circular17 <circular@operamail.com>
2016-08-29 12:00:43 +0100

    moved blur filters in separate unit

--- bgrabitmap/bgrabitmappack.pas
+++ bgrabitmap/bgrabitmappack.pas
@@ -22,7 +22,8 @@ uses
   BGRAColorQuantization, BGRADithering, BGRAUTF8, BGRALCLBitmap, BGRAWritePNG, 
   BGRAGifFormat, BGRAGraphics, BGRASceneTypes, BGRARenderer3D, 
   BGRAWriteBmpMioMap, BGRAOpenGLType, BGRASpriteGL, BGRAOpenGL, BGRACanvasGL, 
-  BGRAFontGL, BGRAOpenGL3D, BGRAPhoxo, BGRAFilterScanner, BGRAFilterType;
+  BGRAFontGL, BGRAOpenGL3D, BGRAPhoxo, BGRAFilterScanner, BGRAFilterType, 
+  BGRAFilterBlur;
 
 implementation
 
