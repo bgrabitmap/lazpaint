4ae3f2fa8f5535e55ae9c504c247bf6cd25eec84
circular17 <circular@operamail.com>
2019-10-18 15:32:49 +0200

    staging batch for windows

---
+++ lazpaint/release/windows/stage.bat
@@ -0,0 +1,53 @@
+@echo off
+if not exist lazpaint.iss goto :baddir
+
+echo Cleaning previous staging files...
+if exist lazpaint32 del /s /q lazpaint32 >nul
+if exist lazpaint32\i18n rmdir lazpaint32\i18n
+if exist lazpaint32\models rmdir lazpaint32\models
+if exist lazpaint32 rmdir lazpaint32
+if exist lazpaint64 del /s /q lazpaint64 >nul
+if exist lazpaint64\i18n rmdir lazpaint64\i18n
+if exist lazpaint64\models rmdir lazpaint64\models
+if exist lazpaint64 rmdir lazpaint64
+
+echo Binary found:
+dir /b ..\bin\lazpaint*.exe
+echo.
+if not exist ..\bin\lazpaint32.exe goto :missingbin32
+echo Staging 32-bit version...
+if not exist lazpaint32 mkdir lazpaint32
+copy ..\bin\lazpaint32.exe lazpaint32\lazpaint.exe >nul
+copy ..\bin\readme.txt lazpaint32 >nul
+copy ..\bin\*.ini lazpaint32 >nul
+if not exist lazpaint32\i18n mkdir lazpaint32\i18n
+copy ..\bin\i18n\lazpaint.* lazpaint32\i18n >nul
+copy ..\bin\i18n\lclstrconsts.* lazpaint32\i18n >nul
+if not exist lazpaint32\models mkdir lazpaint32\models
+copy ..\bin\models lazpaint32\models >nul
+goto donebin32
+:missingbin32
+echo Error: 32-bit binary not found
+:donebin32
+
+if not exist ..\bin\lazpaint_x64.exe goto :missingbin64
+echo Staging 64-bit version...
+if not exist lazpaint64 mkdir lazpaint64
+copy ..\bin\lazpaint_x64.exe lazpaint64\lazpaint.exe >nul
+copy ..\bin\readme.txt lazpaint32 >nul
+copy ..\bin\*.ini lazpaint32 >nul
+if not exist lazpaint64\i18n mkdir lazpaint64\i18n
+copy ..\bin\i18n\lazpaint.* lazpaint64\i18n >nul
+copy ..\bin\i18n\lclstrconsts.* lazpaint64\i18n >nul
+if not exist lazpaint64\models mkdir lazpaint64\models
+copy ..\bin\models lazpaint64\models >nul
+goto donebin64
+:missingbin64
+echo Error: 64-bit binary not found
+:donebin64
+
+goto end
+:baddir
+echo Error: must be launch from its directory
+:end
+pause
