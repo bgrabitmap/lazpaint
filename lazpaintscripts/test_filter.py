f25c600646ab403ccf0a7d672a3bf479bad73a15
johann <circular@operamail.com>
2019-10-30 16:43:44 +0100

    renaming filters

--- lazpaintscripts/test_filter.py
+++ lazpaintscripts/test_filter.py
@@ -1,11 +1,11 @@
-from lazpaint import filter, image
+from lazpaint import filters, image
 
-#filter.filter_function(green="green*2")
-#filter.phong(color_source=filter.PHONG_COLOR_LAYER, light_x_percent=20, light_y_percent=20, altitude_percent=30, altitude_source=filter.PHONG_ALTITUDE_ALPHA_CHANNEL)
-#filter.emboss(angle=45, transparent=False, preserve_colors=True)
-#filter.noise(grayscale=True, opacity=128)
-#filter.pixelate(20, filter.PIXELATE_QUALITY_BEST)
-#filter.twirl(radius=min(image.get_width(),image.get_height())/2, angle=360)
-#filter.blur_motion(50, 45, True)
-#filter.sharpen(1)
-filter.wave_displacement(100,30,0,25,25)
+#filters.filter_function(green="green*2")
+#filters.phong(color_source=filter.PHONG_COLOR_LAYER, light_x_percent=20, light_y_percent=20, altitude_percent=30, altitude_source=filter.PHONG_ALTITUDE_ALPHA_CHANNEL)
+#filters.emboss(angle=45, transparent=False, preserve_colors=True)
+#filters.noise(grayscale=True, opacity=128)
+#filters.pixelate(20, filter.PIXELATE_QUALITY_BEST)
+#filters.twirl(radius=min(image.get_width(),image.get_height())/2, angle=360)
+#filters.blur_motion(50, 45, True)
+#filters.sharpen(1)
+filters.wave_displacement(100,30,0,25,25)
--- lazpaintscripts/test_layer2.py
+++ lazpaintscripts/test_layer2.py
@@ -1,6 +1,6 @@
-from lazpaint import image, layer, filter
+from lazpaint import image, layer, filters
 
 for i in range(1, image.get_layer_count()+1):
   image.select_layer_index(i)
-  filter.twirl(radius=min(image.get_width(),image.get_height())/2, angle=360)
+  filters.twirl(radius=min(image.get_width(),image.get_height())/2, angle=360)
 
