3414b7cea0767863744e00545c73dcc37f6733d5
johann <circular@operamail.com>
2020-02-18 23:00:10 +0100

    script render lava
    undo, new layer, rename

--- scripts/render_lava.py
+++ scripts/render_lava.py
@@ -1,8 +1,12 @@
-from lazpaint import layer, filters, colors
+from lazpaint import image, layer, filters, colors
 
+image.do_begin()
+layer.new()
 filters.run(filters.RENDER_WOOD)
 layer.new()
 filters.run(filters.RENDER_WATER)
 layer.set_blend_op(layer.BLEND_LINEAR_SUBTRACT)
 layer.merge_over()
+layer.set_name("Lava")
+image.do_end()
 
