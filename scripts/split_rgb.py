ab4b733df4ef34797294434a5c654a582c8cb23b
johann <circular@operamail.com>
2020-02-19 13:32:32 +0100

    script: split RGB using Pillow

--- scripts/split_rgb.py
+++ scripts/split_rgb.py
@@ -1,44 +1,52 @@
 # Split RGB channels
-from lazpaint import image, layer, tools, colors
+from lazpaint import image, dialog, layer
+from PIL import Image
 
-image.do_begin()
+temp_name = image.get_temporary_name()
+temp_name = layer.save_as(temp_name)
+if temp_name is None:
+  dialog.show_message("Failed to retrieve layer")
+  exit()
 
 layer_id = layer.get_id()
-layer.duplicate()
-layer.new()
-tools.choose(tools.FLOOD_FILL)
-tools.set_fore_color(colors.BLUE)
-tools.mouse((0,0))
-layer.set_blend_op(layer.BLEND_DARKEN)
-layer.merge_over()
-layer.set_blend_op(layer.BLEND_LIGHTEN)
-layer.set_name("Blue channel")
+layer_index = image.get_layer_index()
+layer_opacity = layer.get_opacity()
+im = Image.open(temp_name)
+width, height = im.size
 
-layer.select_id(layer_id)
-layer.duplicate()
-image.move_layer_index(image.get_layer_index(), image.get_layer_count())
-layer.new()
-tools.choose(tools.FLOOD_FILL)
-tools.set_fore_color(colors.LIME)
-tools.mouse((0,0))
-layer.set_blend_op(layer.BLEND_DARKEN)
-layer.merge_over()
-layer.set_blend_op(layer.BLEND_LIGHTEN)
-layer.set_name("Green channel")
+zero = Image.new("L", (width, height))
+if im.mode == "RGBA":
+  alpha = im.getchannel("A")
+else:
+  alpha = Image.new("L", (width, height), 255)
 
-layer.select_id(layer_id)
-layer.duplicate()
-image.move_layer_index(image.get_layer_index(), image.get_layer_count())
-layer.new()
-tools.choose(tools.FLOOD_FILL)
-tools.set_fore_color(colors.RED)
-tools.mouse((0,0))
-layer.set_blend_op(layer.BLEND_DARKEN)
-layer.merge_over()
-layer.set_blend_op(layer.BLEND_LIGHTEN)
-layer.set_name("Red channel")
+r = Image.merge("RGBA", [im.getchannel("R"), zero, zero, alpha])
+g = Image.merge("RGBA", [zero, im.getchannel("G"), zero, alpha])
+b = Image.merge("RGBA", [zero, zero, im.getchannel("B"), alpha])
+
+image.do_begin()
 
 layer.select_id(layer_id)
 layer.set_visible(False)
 
+channels = [(r, "Red", "R"), (g, "Green", "G"), (b, "Blue", "B")]
+if im.mode == "RGBA":
+  a = Image.merge("RGBA", [zero, zero, zero, alpha])
+  channels.append((a, "Alpha", "A"))
+
+channels_id = []
+for ch in channels:
+  ch[0].save(temp_name, "BMP")
+  layer.add_from_file(temp_name)
+  layer.set_name(ch[1] + " channel")
+  layer.set_opacity(layer_opacity)
+  if ch[2] != "A":
+    layer.set_blend_op(layer.BLEND_LIGHTEN)
+  layer.set_registry("split-channel", ch[2])
+  layer.set_registry("split-source-id", layer_id)
+  channels_id.append(layer.get_id())
+  image.move_layer_index(image.get_layer_index(), layer_index+1)
+
+layer.select_id(layer_id)
+layer.set_registry("split-channels-id", channels_id)
 image.do_end()
