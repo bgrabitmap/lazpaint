255711d8a66993e5c5b896e25f9084d135bd3d86
Johann <circular@operamail.com>
2020-05-20 23:11:07 +0200

    fix file access right, use fakeroot instead of --root-owner-group
    --root-owner-group not available on Raspbian

--- lazpaint/release/debian/makedeb.sh
+++ lazpaint/release/debian/makedeb.sh
@@ -33,21 +33,15 @@ echo "Creating package..."
 
 rm -rf "${STAGING_DIR}"
 mkdir "${STAGING_DIR}"
-chmod 0755 "${STAGING_DIR}"
 
 mkdir "${STAGING_DIR}/DEBIAN"
-chmod 0755 "${STAGING_DIR}/DEBIAN"
 cp "debian/control" "${STAGING_DIR}/DEBIAN"
 sed -i -e "s/Architecture: any/Architecture: ${TARGET_ARCHITECTURE}/" "${STAGING_DIR}/DEBIAN/control"
 
 mkdir "${USER_DIR}"
-chmod 0755 "${USER_DIR}"
 
 mkdir "${BIN_DIR}" 
-chmod 0755 "${BIN_DIR}" 
 cp "${SOURCE_BIN}/lazpaint" "${BIN_DIR}"
-chmod 0755 "${BIN_DIR}/lazpaint"
-chmod +x "${BIN_DIR}/lazpaint"
 echo "Determining dependencies..."
 dpkg-shlibdeps "${BIN_DIR}/lazpaint"
 DEPENDENCIES="$(sed -n 's/^shlibs:Depends=//p' debian/substvars)"
@@ -56,47 +50,38 @@ rm "debian/substvars"
 echo "Done determining dependencies."
 
 mkdir "${SHARE_PARENT_DIR}"
-chmod 0755 "${SHARE_PARENT_DIR}" 
 
 mkdir "${DOC_PARENT_DIR}"
-chmod 0755 "${DOC_PARENT_DIR}"
 mkdir "${DOC_DIR}"
-chmod 0755 "${DOC_DIR}"
 gzip -9 -n -c "debian/changelog" >"${DOC_DIR}/changelog.gz"
 cp "debian/copyright" "${DOC_DIR}"
 cp "${SOURCE_BIN}/readme.txt" "${DOC_DIR}/README"
 
 cp -r applications "${SHARE_PARENT_DIR}"
-chmod 0755 "${SHARE_PARENT_DIR}/applications"
 cp -r pixmaps "${SHARE_PARENT_DIR}"
-chmod 0755 "${SHARE_PARENT_DIR}/pixmaps"
 cp -r "man" "${SHARE_PARENT_DIR}" 
-chmod 0755 "${SHARE_PARENT_DIR}/man"
-chmod 0755 "${SHARE_PARENT_DIR}/man/man1"
 gzip -9 -n "${SHARE_PARENT_DIR}/man/man1/lazpaint.1" 
 
 mkdir "${SHARE_DIR}"
-chmod 0755 "${SHARE_DIR}"
 cp "${SOURCE_BIN}/lazpaint_simplified.ini" "${SHARE_DIR}"
 
 mkdir "${SHARE_DIR}/i18n"
-chmod 0755 "${SHARE_DIR}/i18n"
 find "${SOURCE_BIN}/i18n" -maxdepth 1 -name *.po -exec cp {} "${SHARE_DIR}/i18n" \;
 
 cp -r "${SOURCE_BIN}/models" "${SHARE_DIR}/models"
-chmod 0755 "${SHARE_DIR}/models"
 
 mkdir "${SCRIPT_DIR}"
-chmod 0755 "${SCRIPT_DIR}"
 find "${SOURCE_SCRIPT_DIR}" -maxdepth 1 -name *.py -exec cp {} "${SCRIPT_DIR}" \;
 mkdir "${SCRIPT_DIR}/lazpaint" 
-chmod 0755 "${SCRIPT_DIR}/lazpaint"
 find "${SOURCE_SCRIPT_DIR}/lazpaint" -maxdepth 1 -name *.py -exec cp {} "${SCRIPT_DIR}/lazpaint" \;
 
 SIZE_IN_KB="$(du -s ${STAGING_DIR} | awk '{print $1;}')"
 echo "Installed-Size: ${SIZE_IN_KB}" >> "${STAGING_DIR}/DEBIAN/control"
+find "${STAGING_DIR}" -type d -exec chmod 0755 {} \;
+find "${STAGING_DIR}" -type f -exec chmod 0644 {} \;
+chmod 0755 "${BIN_DIR}/lazpaint"
 
-dpkg-deb --root-owner-group --build "${STAGING_DIR}" "${PACKAGE_NAME}.deb"
+fakeroot dpkg-deb --build "${STAGING_DIR}" "${PACKAGE_NAME}.deb"
 
 NO_INSTALL_ARCHIVE="${PACKAGE_NAME}_no_install.tar.gz"
 
