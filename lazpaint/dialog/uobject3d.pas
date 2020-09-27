ffe65147f34ae5c517ef5e1c77f8eb2394729ec1
Johann ELSASS <circular@operamail.com>
2020-09-27 12:04:00 +0200

    retina scaling for import 3d obj

--- lazpaint/dialog/uobject3d.pas
+++ lazpaint/dialog/uobject3d.pas
@@ -325,7 +325,7 @@ end;
 procedure TFObject3D.BGRAView3DRedraw(Sender: TObject;
   Bitmap: TBGRABitmap);
 begin
-  DrawCheckers(Bitmap,rect(0,0,Bitmap.Width,Bitmap.Height));
+  DrawCheckers(Bitmap,rect(0,0,Bitmap.Width,Bitmap.Height),CanvasScale);
   scene.Surface := Bitmap;
   scene.Render;
 end;
@@ -463,6 +463,7 @@ end;
 procedure TFObject3D.FormCreate(Sender: TObject);
 begin
   ScaleControl(Self,OriginalDPI);
+  BGRAView3D.BitmapAutoScale:= false;
 
   CheckOKCancelBtns(Button_OK,Button_Cancel);
   CheckSpinEdit(SpinEdit_ColorOpacity);
